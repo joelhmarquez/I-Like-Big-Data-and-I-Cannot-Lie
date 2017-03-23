ceb7c80fc0ffe0487abf6249af00e79827e7e662
Erik Eakins <eeakins06@gmail.com>
2017-03-22 21:51:21 -0600

    Updated insertBolt to insert data correctly into Cassandra. Cassandra IP modified to 0.0.0.0 for security.

--- InsertBolt/insertTweet.py
+++ InsertBolt/insertTweet.py
@@ -1,50 +1,63 @@
-import storm
+#import storm
 import json
 import math
 # from cassandra.io.libevreactor import LibevConnection
 # Previous not built for Windows
 from cassandra.cluster import Cluster
+from cassandra.auth import PlainTextAuthProvider
 
-CREATE_COLUMNS = "id text PRIMARY KEY, text text, lat text, lng text, time text, location text, score int, state text"
+CREATE_COLUMNS = "id text PRIMARY KEY, tweettext text, lat text, lng text, time text, location text, score int, state text"
 
-class insertTweetData(storm.BasicBolt):
+class insertTweetData():#storm.BasicBolt):
     global CREATE_COLUMNS
 
-    def initialize(self,tweet):
+    def __init__(self,tweet):
+        self.text = tweet['tweettext']
         self.state = tweet['state']
         self.time = int(tweet['time'])
-        self.time = floor(self.time/3600)
+        self.time = int(math.floor(self.time//3600))
 
     def insert(self,tweet):
-        # Need IP address for Cassandra Server
         # Need to figure out authentication for connecting to server
-        cluster = Cluster('192.168.1.1')
+        # authProvider = PlainTextAuthProvider(username='internal', password='fuckapplebees')
+        cluster = Cluster(["0.0.0.0"],port=9042)#,protocol_version=2,auth_provider=authProvider)
         # cluster.connection_class = LibevConnection
         session = cluster.connect()
-        #selectDB = "USE "+self.state+";"
-        insertData = "INSERT INTO "+self.state+"."+self.time+" JSON "+json.dumps(tweet)+";"
-        #session.execute(selectDB)
+        session.execute("CREATE KEYSPACE IF NOT EXISTS "+self.state+" WITH replication = {'class':'SimpleStrategy', 'replication_factor':3};")
+        selectDB = "USE "+self.state+";"
+        insertData = "INSERT INTO \""+str(self.time)+"\" JSON '"+json.dumps(tweet)+"';"
+        session.execute(selectDB)
         session.set_keyspace(self.state)
         try:
+            print(insertData)
             session.execute(insertData)
-            break
+
         except Exception as e:
-            print("Error inserting data {}, {}".format(e.errno, e.strerror))
-            createTable = "CREATE TABLE "+self.time+"("+CREATE_COLUMNS+");"
+            print("Error inserting data")
+            createTable = "CREATE TABLE \""+str(self.time)+"\"("+CREATE_COLUMNS+");"
+            print(createTable)
             try:
                 session.execute(createTable)
-                break
                 try:
+                    print(insertData)
                     session.execute(insertData)
-                    break
+                   
                 except Exception as e:
-                    print("Error inserting data after creating table {} {}".format(e.errno, e.strerror))
-                    break
+                    print("Error inserting data after creating table")
+
             except Exception as e:
-                print("Error creating table {} {}".format(e.errno, e.strerror))
-                break
+                print("Error creating table")
 
         cluster.shutdown()
 
-            
-insertTweetData().run()
+
+#insertTweetData().run()
+
+# def main():
+#     tweet = json.load(open("sampleTweets1.txt","r"))
+#     tweet[0]['tweettext'] = tweet[0]['text']
+#    # tweet[0]['retweetcount'] = tweet[0]['retweetCount']
+#    # del tweet[0]['retweetCount']
+#     del tweet[0]['text']
+#     tweet = tweet[0]
+#     testing = insertTweetData(tweet)
