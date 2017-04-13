3f160e4b7bed9cbf6a295054a97d6d2efb4d177d
Christina Nguyen <cq_nguyen_vn@yahoo.com>
2017-04-13 13:03:15 -0600

    Update (still not functional yet)

--- navbar.elm
+++ navbar.elm
@@ -1,6 +1,28 @@
 module Base.Navbar exposing (..)
 
-import Bootstrap.Navbar exposing(..)
+import Bootstrap.Navbar as Navbar
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (..)
+
+type alias Model = 
+    { navbarState : Navbar.State }
+
+initState : (Model, Cmd Msg)
+initState toMsg = 
+    let
+        (navbarState, navbarCmd) = Navbar.initState NavbarMsg
+    in 
+        ({ navbarState = navbarState }, navBarCmd )
+
+type Msg = NavbarMsg Navbar.State
+
+update : Msg -> MOdel -> (Model, Cmd Msg)
+update msg model =
+    case msg of 
+        NavbarMsg state ->
+            ( { model | navbarState = state }, Cmd.none)
+
+view : Model -> Html Msg
+view model = 
+    Navbar.config NavbarMsg
