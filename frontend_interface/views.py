6449d6de0114c4d49061b76f79b3e0c1c844655f
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-16 06:54:51 +0000

    Mapping the django app urls to the django project.

--- frontend_interface/views.py
+++ frontend_interface/views.py
@@ -3,4 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+	return HttpResponse("Hello, world. You're at the main index.")
--- ilbdaicl/settings.py
+++ ilbdaicl/settings.py
@@ -119,3 +119,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+
+
+APPEND_SLASH = True
