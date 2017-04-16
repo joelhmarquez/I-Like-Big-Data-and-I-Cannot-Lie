593c9c52f12adeec55863af95a6c4d66af1d8b66
Michael V. Swisher <michael.v.swisher@colorado.edu>
2017-04-16 04:18:11 -0600

    Adding in state codes for map retrieval. Going to test on server to make sure it adds things correctly

--- Devops/mapRetrieval.py
+++ Devops/mapRetrieval.py
@@ -9,7 +9,9 @@ session = cluster.connect()
 session.row_factory = dict_factory
 
 statesToInsert=['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'newhampshire', 'newjersey', 'newmexico', 'newyork', 'northcarolina', 'northdakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhodeisland', 'southcarolina', 'southdakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'westvirginia', 'wisconsin', 'wyoming']
-
+stateToCode = {"alabama":"US-AL","alaska":"US-AK","arizona":"US-AZ","arkansas":"US-AR","california":"US-CA","colorado":"US-CO","connecticut":"US-CT","delaware":"US-DE","florida":"US-FL","georgia":"US-GA","hawaii":"US-HI","idaho":"US-ID","illinois":"US-IL","indiana":"US-IN","iowa":"US-IA","kansas":"US-KS","kentucky":"US-KY","louisiana":"US-LA","maine":"US-ME","maryland":"US-MD","massachusetts":"US-MA","michigan":"US-MI","minnesota":"US-MN","mississippi":"US-MS","missouri":"US-MO","montana":"US-MT","nebraska":"US-NE","nevada":"US-NV","newhampshire":"US-NH","newjersey":"US-NJ","newmexico":"US-NM","newyork":"US-NY","northcarolina":"US-NC","northdakota":"US-ND","ohio":"US-OH","oklahoma":"US-OK","oregon":"US-OR","pennsylvania":"US-PA","rhodeisland":"US-RI","southcarolina":"US-SC","southdakota":"US-SD","tennessee":"US-TN","texas":"US-TX","utah":"US-UT","vermont":"US-VT","virginia":"US-VA","washington":"US-WA","westvirginia":"US-WV","wisconsin":"US-WI","wyoming":"US-WY"}
+          
+finalMapScore = [['State', 'Hate Score']]
 for state in statesToInsert:
 	#Handle broken keyspace names
 	try:
@@ -24,22 +26,22 @@ for state in statesToInsert:
 					#General exception handling
 					try:
 						countQuery = "select \"sentimentscore\" from \"%s\" where id = \'0\';" % timeblock
-						countResult = str(session.execute(countQuery)[0]['sentimentscore'])
+						countResult = session.execute(countQuery)[0]['sentimentscore']
 
 					except Exception as e:
-						countResult = "0.0"
+						countResult = 0.0
 						print "Query Error: %s" % e
 									
 		except Exception as e:
-			countResult = "0.0"
+			countResult = 0.0
 			print e
 
 	except Exception as e:
-		countResult = "0.0"
+		countResult = 0.0
 		print e
 
 
 
-	print timeblock, countResult, state
+	finalMapScore.append([state, countResult])
 
 cluster.shutdown()
