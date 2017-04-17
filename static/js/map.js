ca8cafc8d5639003f3bd53ec7a8fd8d582c2b7aa
Joe Marquez <joma6391@colorado.edu>
2017-04-17 13:58:23 -0600

    setting up rest calls

--- static/js/map.js
+++ static/js/map.js
@@ -5,25 +5,25 @@
 'use strict';
 
 (function () {
-    google.charts.load('current', {'packages':['geochart']});
-    google.charts.setOnLoadCallback(drawRegionsMap);
-    var states = {
-        'Alabama': 1, 'Alaska': 100, 'Arizona': 100, 'Arkansas': 100, 'California': 100,
-        'Colorado': 100, 'Connecticut': 100, 'Delaware': 100, 'Florida': 100, 'Georgia': 100,
-        'Hawaii': 100, 'Idaho': 100, 'Illinois': 100, 'Indiana': 100, 'Iowa': 100,
-        'Kansas': 100, 'Kentucky': 100, 'Louisiana': 100, 'Maine': 100, 'Maryland': 100,
-        'Massachusetts': 100, 'Michigan': 100, 'Minnesota': 100, 'Mississippi': 100, 'Missouri': 100,
-        'Montana': 100, 'Nebraska': 100, 'Nevada': 100, 'New Hampshire': 100, 'New Jersey': 100,
-        'New Mexico': 100, 'New York': 100, 'North Carolina': 100, 'North Dakota': 100, 'Ohio': 100,
-        'Oklahoma': 100, 'Oregon': 100, 'Pennsylvania': 100, 'Rhode Island': 100, 'South Carolina': 100,
-        'South Dakota': 100, 'Tennessee': 100, 'Texas': 100, 'Utah': 100, 'Vermont': 100,
-        'Virginia': 100, 'Washington': 100, 'West Virginia': 100, 'Wisconsin': 100, 'Wyoming': 100
-    };
     var chart;
 
-    function drawRegionsMap() {
-        var array = [['State', 'Score']];
-        getValues(array);
+    init();
+
+    function init() {
+        var url = "http://swishertest.site/api/map";
+        var http = new HttpClient();
+
+        http.get(url,function (resp) {
+            initMap(resp);
+            // console.log(resp)
+        })
+    }
+
+    var initMap = function drawRegionsMap(values) {
+        google.charts.load('current', {'packages':['geochart']});
+        google.charts.setOnLoadCallback(drawRegionsMap);
+
+        var array = ['State', 'Score'];
 
         var data = google.visualization.arrayToDataTable(array);
 
@@ -40,14 +40,6 @@
         google.visualization.events.addListener(chart, 'select', function getSelection() {
             console.log(chart.getSelection())
         });
-    }
-
-    var getValues = function (array) {
-        for (var key in states) {
-            if (states.hasOwnProperty(key)) {
-                array.push([key, states[key]])
-            }
-        }
     }
 
 })();
