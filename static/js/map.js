caa7aa83d031889e9698c3a5e011ecaf55bb6eb3
Joe Marquez <joma6391@colorado.edu>
2017-04-17 14:40:45 -0600

    Finalized rest call

--- static/js/map.js
+++ static/js/map.js
@@ -6,26 +6,20 @@
 
 (function () {
     var chart;
-
-    init();
+    google.charts.load('current', {'packages': ['geochart']});
+    google.charts.setOnLoadCallback(init);
 
     function init() {
         var url = "http://swishertest.site/api/map";
         var http = new HttpClient();
 
-        http.get(url,function (resp) {
-            initMap(resp);
-            // console.log(resp)
+        http.get(url, function(resp) {
+            drawRegionsMap(JSON.parse(resp));
         })
     }
 
-    var initMap = function drawRegionsMap(values) {
-        google.charts.load('current', {'packages':['geochart']});
-        google.charts.setOnLoadCallback(drawRegionsMap);
-
-        var array = ['State', 'Score'];
-
-        var data = google.visualization.arrayToDataTable(array);
+    function drawRegionsMap(values) {
+        var data = google.visualization.arrayToDataTable(values.values);
 
         var options = {
             region: "US",
