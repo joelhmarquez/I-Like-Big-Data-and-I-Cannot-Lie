794fc5c6072a394708080977e13cb9fa06ea4e88
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-17 22:24:34 +0000

    Adding in file to backup cassandra database

---
+++ Devops/backDatThangUp.py
@@ -0,0 +1,14 @@
+#This script performs a dump on all cassandra keyspaces to perform total backup
+
+
+
+import os
+
+
+
+keyspaceNames = ['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'newhampshire', 'newjersey', 'newmexico', 'newyork', 'northcarolina', 'northdakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhodeisland', 'southcarolina', 'southdakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'westvirginia', 'wisconsin', 'wyoming']
+
+
+for name in keyspaceNames:
+        commandToRun = "nodetool -h localhost -p 7199 snapshot %s" % name
+        os.system(commandToRun)
