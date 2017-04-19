b453bf118c58478a673cfb1246f5ef7397331c21
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-19 17:15:57 +0000

    Fixing minor bug in elassandra install script

--- Devops/elassandraInstall.sh
+++ Devops/elassandraInstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 wget http://packages.elassandra.io/deb/elassandra_2.1.1-18.1.4_all.deb
-dpgk -i elassandra_2.1.1-18.1.4_all.deb
+dpkg -i elassandra_2.1.1-18.1.4_all.deb
 sudo apt-get clean
 sudo apt-get install elassandra
