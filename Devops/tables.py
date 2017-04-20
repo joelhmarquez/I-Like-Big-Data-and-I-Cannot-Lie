c1d3030860ca13b8d5c66249732ff1648e499c57
amber-womack <amber.n.womack@gmail.com>
2017-04-19 19:10:52 -0600

    adding table.py

--- Devops/tables.py
+++ Devops/tables.py
@@ -1,65 +1,65 @@
 from cassandra.cluster import Cluster
-cluster = Cluster(contact_point=['172.31.35.21'], port=9042)
-session = cluster.connect()
-
-statesToInsert = {
-        'AK',
-        'AL',
-        'AR',
-        'AZ',
-        'CA',
-        'CO',
-        'CT',
-        'DC',
-        'DE',
-        'FL',
-        'GA',
-        'HI',
-        'IA',
-        'ID',
-        'IL',
-        'IN',
-        'KS',
-        'KY',
-        'LA',
-        'MA',
-        'MD',
-        'ME',
-        'MI',
-        'MN',
-        'MO',
-        'MS',
-        'MT',
-        'NC',
-        'ND',
-        'NE',
-        'NH',
-        'NJ',
-        'NM',
-        'NV',
-        'NY',
-        'OH',
-        'OK',
-        'OR',
-        'PA',
-        'RI',
-        'SC',
-        'SD',
-        'TN',
-        'TX',
-        'UT',
-        'VA',
-        'VI',
-        'VT',
-        'WA',
-        'WI',
-        'WV',
-        'WY'
+cluster = Cluster(['172.31.3.194'], port=9042)
+try:
+        session = cluster.connect()
+except:
+        print('failed to connect')
+statesToInsert={
+    'alabama',
+    'alaska',
+    'arizona',
+    'arkansas',
+    'california',
+    'colorado',
+    'connecticut',
+    'delaware',
+    'florida',
+    'georgia',
+    'hawaii',
+    'idaho',
+    'illinois',
+    'indiana',
+    'iowa',
+    'kansas',
+    'kentucky',
+    'louisiana',
+    'maine',
+    'maryland',
+    'massachusetts',
+    'michigan',
+    'minnesota',
+    'mississippi',
+    'missouri',
+    'montana',
+    'nebraska',
+    'nevada',
+    'newhampshire',
+    'newjersey',
+    'newmexico',
+    'newyork',
+    'northcarolina',
+    'northdakota',
+    'ohio',
+    'oklahoma',
+    'oregon',
+    'pennsylvania',
+    'rhodeisland',
+    'southcarolina',
+    'southdakota',
+    'tennessee',
+    'texas',
+    'utah',
+    'vermont',
+    'virginia',
+    'washington',
+    'westvirginia',
+    'wisconsin',
+    'wyoming'
 }
 
 for item in statesToInsert:
+        try:
         #insert into the table
-        session.execute("CREATE KEYSPACE IF NOT EXISTS " + item + " WITH replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };")
-
-
-cluster.shutdown()
+                session.execute("CREATE KEYSPACE IF NOT EXISTS " + item + " WITH replication = { 'class' : 'SimpleStrategy', 'replication_factor' : 3 };")
+        except:
+                print "database not working: %s" % item
