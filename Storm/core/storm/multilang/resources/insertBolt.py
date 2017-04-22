fd4b00c8931ab5952ff3cae3531bb36107889a13
Erik Eakins <erik.eakins@colorado.edu>
2017-04-21 20:28:54 -0600

    Adding print statements to emit if success or error after insert.py

--- Storm/core/storm/multilang/resources/insertBolt.py
+++ Storm/core/storm/multilang/resources/insertBolt.py
@@ -13,16 +13,11 @@ class insertTweetData(storm.BasicBolt):
 
     def process(self,tup):
         tweet = tup.values[0]
-        #tweet['tweet'] = tweet['tweet'].replace("'","")
-        #tweet['tweet'] = tweet['tweet'].replace("\"","")
-        #tweet['tweet'] = tweet['tweet'].replace("\/","")
+
         self.id = tweet['id']
         self.text = tweet['tweettext']
         self.text = self.text.replace("'","")
         self.text = self.text.replace("\"","")
-        #self.text = self.text.replace("\\"," ")
-        #self.text = self.text.replace("@","AT:")
-        #self.text = re.sub(r'^https?:\/\/.*[\r\n]*', '', self.text, flags=re.MULTILINE)
         self.lat = tweet['lat']
         self.lng = tweet['lng']
         self.location = tweet['location']
@@ -35,18 +30,20 @@ class insertTweetData(storm.BasicBolt):
     def insert(self,tweet):
         # Need to figure out how to emit an error properly for logging
         cluster = Cluster(["172.31.3.194"],port=9042)
-        # cluster.connection_class = LibevConnection
+
         try:
             session = cluster.connect()
             selectDB = "USE "+self.state+";"
-            #insertData = "INSERT INTO \""+str(self.time)+"\" JSON '"+json.dumps(tweet)+"';"
             insertData = "INSERT INTO \""+str(self.time)+"\" (id, tweettext, lat, lng, time, location, sentimentscore, state) VALUES (\'"+self.id+"\',\'"+self.text+"\',\'"+self.lat+"\',\'"+self.lng+"\',\'"+tweet['time']+"\',\'"+self.location+"\',\'"+self.score+"\',\'"+self.state+"\');"
+            
             try:
                 session.execute(selectDB)
 
                 try:
                     session.execute(insertData)
-
+                    success = "Successfully inserted"
+                    storm.emit([sucess])
+                
                 except Exception as e:
                     createTable = "CREATE TABLE \""+str(self.time)+"\"("+CREATE_COLUMNS+");"
 
@@ -54,19 +51,22 @@ class insertTweetData(storm.BasicBolt):
                         session.execute(createTable)
                         try:
                             session.execute(insertData)
+                            success = "Successfully inserted"
+                            storm.emit([sucess])
                    
                         except Exception as e:
                             error = "Unable to insert data. Query: "+insertData
-
+                            storm.emit([error])
                     except Exception as e:
                         error = "Unable to CREATE TABLE "+str(self.time)
-
+                        storm.emit([error])
             except Exception as e:
                 error = "Unable to select keyspace "+self.state
-
+                storm.emit([error])
         except Exception as e:
             error = "Unable to connect to Cassandra"
-            
+            storm.emit([error])
+        
         cluster.shutdown()
 
 
