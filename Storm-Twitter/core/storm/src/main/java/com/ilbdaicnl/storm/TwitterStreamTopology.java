c58660fc21070d620c576ab059ba9df18705e26d
Joe Marquez <joma6391@colorado.edu>
2017-04-24 14:50:03 -0600

    renamed storm to avoid overide on merge

---
+++ Storm-Twitter/core/storm/src/main/java/com/ilbdaicnl/storm/TwitterStreamTopology.java
@@ -0,0 +1,51 @@
+package com.ilbdaicnl.storm;
+
+import org.apache.storm.utils.Utils;
+
+import com.ilbdaicnl.resources.ResourceMgr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+
+public class TwitterStreamTopology {
+	public static void main(String[] args) {		
+        String consumerKey = "";
+        String consumerSecret = "";
+        String accessToken = "";
+        String accessTokenSecret = "";
+        List<String> keyWords = new ArrayList<String>();
+        
+        ResourceMgr resourceMgr = ResourceMgr.getInstance();
+        
+        consumerKey = resourceMgr.getConsumerKey();
+        consumerSecret = resourceMgr.getConsumerSecret();
+        accessToken = resourceMgr.getAccessToken();
+        accessTokenSecret = resourceMgr.getAccessTokenSecret();
+        keyWords = resourceMgr.getKeyWords();
+        
+        TopologyBuilder builder = new TopologyBuilder();
+
+        builder.setSpout("twitter", new TwitterStreamSpout(consumerKey, consumerSecret,
+                accessToken, accessTokenSecret, keyWords.toArray(new String[0])));
+        builder.setBolt("formatter", new TweetFormatterBolt()).shuffleGrouping("twitter");
+        builder.setBolt("geolocation", new GeolocationBolt()).shuffleGrouping("formatter");
+//        builder.setBolt("sentiment", new SentimentAnalysisBolt()).shuffleGrouping("geolocation", "success");
+//        builder.setBolt("print", new TwitterStreamPrint()).shuffleGrouping("geolocation", "failure");
+//        builder.setBolt("insert", new CassandraInsertBolt()).shuffleGrouping("sentiment");
+//        builder.setBolt("log", new LoggerBolt()).shuffleGrouping("insert", "error");
+        builder.setBolt("print", new TwitterStreamPrint()).shuffleGrouping("geolocation", "success");
+
+
+        Config conf = new Config();
+
+
+        LocalCluster cluster = new LocalCluster();
+
+        cluster.submitTopology("racialMapping", conf, builder.createTopology());
+
+    }
+}
