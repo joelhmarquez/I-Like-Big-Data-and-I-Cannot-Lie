9fd2d8d9223931f2ddd7e0bce9c5a68a1d4e1656
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-24 19:37:59 +0000

    Setting up settings to handle rest api structure. Also updating to handle templating

--- ilbdaicl/settings.py
+++ ilbdaicl/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,6 +123,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'staticfiles'),
+)
 
 
 
