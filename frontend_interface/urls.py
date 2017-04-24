3aaf59946532f803c20850b49a3e525db52a8e90
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-24 18:59:32 +0000

    Beginning to setup the urls for the main app

--- frontend_interface/urls.py
+++ frontend_interface/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import url
 
 from . import views
+from .views import HomePageView, TeamPageView, TechnologiesPageView
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
+	url(r'^$', HomePageView.as_view(), name='index'),
+	url(r'^about$', TeamPageView.as_view(), name='team'),
+	url(r'^tech$', TechnologiesPageView.as_view(), name='tech'),
 	url(r'api/map$', views.mapsData, name='mapsData'),
 ]
