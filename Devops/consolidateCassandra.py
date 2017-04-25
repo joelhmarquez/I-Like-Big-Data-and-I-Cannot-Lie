5c376c0f98efd60f1065f4aaa7a021b2b0036fda
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-25 22:05:30 +0000

    Finished the migration script. It executed flawlessly.........like me &#3232;\_&#3232;

--- Devops/consolidateCassandra.py
+++ Devops/consolidateCassandra.py
@@ -15,6 +15,8 @@ newSession.row_factory = dict_factory
 
 statesToInsert=['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'newhampshire', 'newjersey', 'newmexico', 'newyork', 'northcarolina', 'northdakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhodeisland', 'southcarolina', 'southdakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'westvirginia', 'wisconsin', 'wyoming']
 
+newSession.set_keyspace('twittertweets')
+
 for state in statesToInsert:
 	#Handle broken keyspace names
 	try:
@@ -29,11 +31,24 @@ for state in statesToInsert:
 					dbQuery = "select * from \"%s\";" % str(timeblock)
 					for result in session.execute(dbQuery):
 						if result['id'] != '0':
+							
 							if len(result['id']) > 18:
 								#result['id'] = re.search('.{13}$', result['id'])
+
 								result['id'] = re.search('.{18}$', str(result['id'])).group(0)
 
-								print result
+								newid = result['id']
+								tweettext = result['tweettext']
+								lat = result['lat']
+								lng = result['lng']
+								time = result['time']
+								location = result['location']
+								sentimentscore = result['sentimentscore']
+								newstate = result['state']
+
+								insertQuery = "INSERT INTO "+newstate+" (id, tweettext, lat, lng, time, location, sentimentscore, state) VALUES (\'"+newid+"\',\'"+tweettext+"\',\'"+lat+"\',\'"+lng+"\',"+str(time)+",\'"+location+"\',"+str(sentimentscore)+",\'"+newstate+"\');"
+
+								newSession.execute(insertQuery)
 						#dbResult.append(result)
 						#insertQuery = "INSERT INTO \"%s\" JSON \'{\"id\":\"0\", \"sentimentscore\":\"%s\"}\';" % (timeblock, overallScore)
 						#session.execute(insertQuery)
@@ -46,6 +61,7 @@ for state in statesToInsert:
 					'''
 				
 				except Exception as e:
+					print "Error with insertion: "
 					print e
 								
 		except Exception as e:
