b6ae9f44d8d643f54b1d4709b8ba5e8d2a8a254b
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-25 21:53:14 +0000

    Setting up to begin inserting into the new db

--- Devops/consolidateCassandra.py
+++ Devops/consolidateCassandra.py
@@ -7,7 +7,11 @@ from cassandra.query import *
 cluster = Cluster(['172.31.3.194'], port=9042)
 session = cluster.connect()
 
+newCluster = Cluster(['172.31.3.194'], port=9042)
+newSession = cluster.connect()
+
 session.row_factory = dict_factory
+newSession.row_factory = dict_factory
 
 statesToInsert=['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'newhampshire', 'newjersey', 'newmexico', 'newyork', 'northcarolina', 'northdakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhodeisland', 'southcarolina', 'southdakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'westvirginia', 'wisconsin', 'wyoming']
 
