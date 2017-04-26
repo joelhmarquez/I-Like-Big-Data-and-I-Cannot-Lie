6434ec501b5c95b4129ea77cb871e8b8276eceae
Michael V. Swisher <Michael.V.Swisher@colorado.edu>
2017-04-26 03:19:28 +0000

    Adding additional dependencies to django environment requirements

--- frontend_interface/urls.py
+++ frontend_interface/urls.py
@@ -5,7 +5,7 @@ from .views import HomePageView, TeamPageView, TechnologiesPageView, AnalyticsPa
 
 urlpatterns = [
 	url(r'^$', HomePageView.as_view(), name='index'),
-	url(r'^about$', TeamPageView.as_view(), name='team'),
+	url(r'^team$', TeamPageView.as_view(), name='team'),
 	url(r'^tech$', TechnologiesPageView.as_view(), name='tech'),
 	url(r'^analytics$', AnalyticsPageView.as_view(), name='analytics'),
 	url(r'api/map$', views.mapsData, name='mapsData'),
