bba6fc39eb66fbcc70cb64ad4b1940413b36b139
Joe Marquez <joma6391@colorado.edu>
2017-04-26 17:23:16 -0600

    fixed state order

--- static/js/factory.js
+++ static/js/factory.js
@@ -5,16 +5,16 @@ let Factory = function () {
     let http = new HttpClient();
 
     this.selections = {
-        0: 'Alabama', 1: 'Alaska', 2: 'Arizona', 3: 'Arkansas', 4: 'California',
-        5: 'Colorado', 6: 'Connecticut', 7: 'Delaware', 8: 'Florida', 9: 'Georgia',
-        10: 'Hawaii', 11: 'Idaho', 12: 'Illinois', 13: 'Indiana', 14: 'Iowa',
-        15: 'Kansas', 16: 'Kentucky', 17: 'Louisiana', 18: 'Maine', 19: 'Maryland',
-        20: 'Massachusetts', 21: 'Michigan', 22: 'Minnesota', 23: 'Mississippi', 24: 'Missouri',
-        25: 'Montana', 26: 'Nebraska', 27: 'Nevada', 28: 'New Hampshire', 29: 'New Jersey',
-        30: 'New Mexico', 31: 'New York', 32: 'North Carolina', 33: 'North Dakota', 34: 'Ohio',
-        35: 'Oklahoma', 36: 'Oregon', 37: 'Pennsylvania', 38: 'Rhode Island', 39: 'South Carolina',
-        40: 'South Dakota', 41: 'Tennessee', 42: 'Texas', 43: 'Utah', 44: 'Vermont',
-        45: 'Virginia', 46: 'Washington', 47: 'West Virginia', 48: 'Wisconsin', 49: 'Wyoming'
+        0: 'Rhode Island', 1: 'Mississippi', 2: 'New York', 3: 'Oklahoma', 4: 'Wyoming',
+        5: 'Minnesota', 6: 'Illinois', 7: 'Arkansas', 8: 'South Carolina', 9: 'Indiana',
+        10: 'Maryland', 11: 'Louisiana', 12: 'Texas', 13: 'New Hampshire', 14: 'Iowa',
+        15: 'Wisconsin', 16: 'Arizona', 17: 'South Dakota', 18: 'Kansas', 19: 'Utah',
+        20: 'Virginia', 21: 'Oregon', 22: 'Connecticut', 23: 'Montana', 24: 'California',
+        25: 'Massachusetts', 26: 'Delaware', 27: 'New Mexico', 28: 'Vermont', 29: 'Georgia',
+        30: 'Pennsylvania', 31: 'North Carolina', 32: 'Florida', 33: 'Hawaii', 34: 'Kentucky',
+        35: 'Alaska', 36: 'Nebraska', 37: 'West Virginia', 38: 'Missouri', 39: 'Ohio',
+        40: 'Alabama', 41: 'Colorado', 42: 'Idaho', 43: 'New Jersey', 44: 'Washington',
+        45: 'Maine', 46: 'Tennessee', 47: 'North Dakota', 48: 'Nevada', 49: 'Michigan'
     };
 
     this.test = {
