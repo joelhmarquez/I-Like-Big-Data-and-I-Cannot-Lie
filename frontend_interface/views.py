28cd0f22dfdf31088fa5e35daee83a01c9872fb0
Erik Eakins <eeakins06@gmail.com>
2017-04-26 17:00:57 -0600

    Map queries totals

--- frontend_interface/views.py
+++ frontend_interface/views.py
@@ -146,46 +146,78 @@ def stateDataQuery(statename):
 def mapSetQuery():
 	cluster = Cluster(['172.31.3.194'], port=9042)
 	session = cluster.connect()
+	statesToInsert = {
+	'Arkansas',
+	'Alabama',
+	'Arizona',
+	'Alaska',
+	'California',
+	'Colorado',
+	'Connecticut',
+	'Delaware',
+	'Florida',
+	'Georgia',
+	'Hawaii',
+	'Iowa',
+	'Idaho',
+	'Illinois',
+	'Indiana',
+	'Kansas',
+	'Kentucky',
+	'Louisiana',
+	'Massachusetts',
+	'Maryland',
+	'Maine',
+	'Michigan',
+	'Minnesota',
+	'Missouri',
+	'Mississippi',
+	'Montana',
+	'NorthCarolina',
+	'NorthDakota',
+	'Nebraska',
+	'NewHampshire',
+	'NewJersey',
+	'NewMexico',
+	'Nevada',
+	'NewYork',
+	'Ohio',
+	'Oklahoma',
+	'Oregon',
+	'Pennsylvania',
+	'RhodeIsland',
+	'SouthCarolina',
+	'SouthDakota',
+	'Tennessee',
+	'Texas',
+	'Utah',
+	'Virginia',
+	'Vermont',
+	'Washington',
+	'Wisconsin',
+	'WestVirginia',
+	'Wyoming'
+	}
 
-	session.row_factory = dict_factory
-
-	statesToInsert=['alabama', 'alaska', 'arizona', 'arkansas', 'california', 'colorado', 'connecticut', 'delaware', 'florida', 'georgia', 'hawaii', 'idaho', 'illinois', 'indiana', 'iowa', 'kansas', 'kentucky', 'louisiana', 'maine', 'maryland', 'massachusetts', 'michigan', 'minnesota', 'mississippi', 'missouri', 'montana', 'nebraska', 'nevada', 'newhampshire', 'newjersey', 'newmexico', 'newyork', 'northcarolina', 'northdakota', 'ohio', 'oklahoma', 'oregon', 'pennsylvania', 'rhodeisland', 'southcarolina', 'southdakota', 'tennessee', 'texas', 'utah', 'vermont', 'virginia', 'washington', 'westvirginia', 'wisconsin', 'wyoming']
-	stateToCode = {"alabama":"Alabama","alaska":"Alaska","arizona":"Arizona","arkansas":"Arkansas","california":"California","colorado":"Colorado","connecticut":"Connecticut","delaware":"Delaware","florida":"Florida","georgia":"Georgia","hawaii":"Hawaii","idaho":"Idaho","illinois":"Illinois","indiana":"Indiana","iowa":"Iowa","kansas":"Kansas","kentucky":"Kentucky","louisiana":"Louisiana","maine":"Maine","maryland":"Maryland","massachusetts":"Massachusetts","michigan":"Michigan","minnesota":"Minnesota","mississippi":"Mississippi","missouri":"Missouri","montana":"Montana","nebraska":"Nebraska","nevada":"Nevada","newhampshire":"New Hampshire","newjersey":"New Jersey","newmexico":"New Mexico","newyork":"New York","northcarolina":"North Carolina","northdakota":"North Dakota","ohio":"Ohio","oklahoma":"Oklahoma","oregon":"Oregon","pennsylvania":"Pennsylvania","rhodeisland":"Rhode Island","southcarolina":"South Carolina","southdakota":"South Dakota","tennessee":"Tennessee","texas":"Texas","utah":"Utah","vermont":"Vermont","virginia":"Virginia","washington":"Washington","westvirginia":"West Virginia","wisconsin":"Wisconsin","wyoming":"Wyoming"}
-
-	finalMapScore = [[str('State'), str('Hate Score')]]
-	for state in statesToInsert:
-		#Handle broken keyspace names
-		try:
-			session.set_keyspace(state)
-			timeblocks = sorted(cluster.metadata.keyspaces[state].tables.keys())
-
-			timeNow = int(time.time())/3600
-			#Handle if the keyspace deosn't have tables yet
-			try:
-				for timeblock in timeblocks:
-					if timeNow - int(timeblock) != 0:
-						#General exception handling
-						try:
-							countQuery = "select \"sentimentscore\" from \"%s\" where id = \'0\';" % timeblock
-							countResult = float("%.2f" % float(session.execute(countQuery)[0]['sentimentscore']))
-
-						except Exception as e:
-							countResult = 0.00
-							#print "Query Error: %s" % e
-
-			except Exception as e:
-				countResult = 0.00
-				#print e
-
-		except Exception as e:
-			countResult = 0.00
-			#print e
-
-
-
-		finalMapScore.append([str(stateToCode[state]), countResult])
-	return finalMapScore
-
-
-
-
+	session.execute("USE twittertweets;")
+	count = 0
+	returnValues = dict()
+	totals = []
+	totals.append(["State", "Hate Score"])
+	for item in statesToInsert:
+	#Insert Tables
+	        neutral = session.execute("select count(*) from "+item+" where sentimentscore=0 allow filtering;")[0]
+	        neutral = str(neutral).split('=')
+	        neutral = neutral[1]
+	        neutral = neutral.replace(")","")
+	        #print(item+" sentiment=0 count="+StateCount)
+	        hate = session.execute("select count(*) from "+item+" where sentimentscore>0 allow filtering;")[0]
+	        hate = str(hate).split('=')
+	        hate = hate[1]
+	        hate = hate.replace(")","")
+	        total = int(neutral) + int(hate)
+	        percentage = float((float(hate)/float(total))*100)
+	        totals.append([item,percentage])
+	        #print(item+" Hate Percentage: {}% Total: {}".format(float((float(hate)/float(total))*100),total))
+	returnValues['values'] = totals
+	cluster.shutdown()
